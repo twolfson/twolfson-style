51d838cd030084bb661e3431747627fa37e0cd61
Todd Wolfson <todd@twolfson.com>
2014-11-20 04:23:57 -0800

    Started on test

--- test/twolfson-style-test.js
+++ test/twolfson-style-test.js
@@ -1,11 +1,8 @@
-var twolfson_style = require('../');
+var assert = require('assert');
+var twolfsonStyle = require('../');
 
 describe('twolfson-style', function () {
-  before(function () {
-
-  });
-
-  it('', function () {
-
+  it('exports a function', function () {
+    assert.strictEqual(typeof twolfsonStyle, 'function');
   });
 });
