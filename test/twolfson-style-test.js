067b0bd909c5f9b2c6940a30b6f0fa5a928a09dd
Todd Wolfson <todd@twolfson.com>
2014-11-20 04:27:13 -0800

    Starting to outline test

--- test/twolfson-style-test.js
+++ test/twolfson-style-test.js
@@ -1,10 +1,18 @@
 // Load in dependencies
 var assert = require('assert');
+var fs = require('fs');
 var twolfsonStyle = require('../');
+
+// Define test utilities
+var testUtils = {
+
+};
 
 // Start our tests
 describe('twolfson-style', function () {
-  it('returns awesome', function () {
-    assert.strictEqual(twolfsonStyle(), 'awesome');
+  describe('installing into a directory', function () {
+    it('adds our .jshintrc and .jscsrc files', function () {
+      assert.strictEqual(twolfsonStyle(), 'awesome');
+    });
   });
 });
