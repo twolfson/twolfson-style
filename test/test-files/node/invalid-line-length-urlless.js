a9112cb0aa01c08172f3a45cc50356b6b825e236
Todd Wolfson <todd@twolfson.com>
2014-11-21 01:38:23 -0800

    Added another test for ourselves

---
+++ test/test-files/node/invalid-line-length-urlless.js
@@ -0,0 +1,5 @@
+// Define our expectations for the test
+exports.expected = /Line must be at most 120 characters/i;
+
+// We don't allow comments over 120 characters without URLs
+// var statement = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
