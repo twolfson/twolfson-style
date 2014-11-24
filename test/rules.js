54f0d03baa66d3533f2932cf9cda41c712d83a7b
Todd Wolfson <todd@twolfson.com>
2014-11-24 00:05:33 -0800

    Adding precheck rules

--- test/rules.js
+++ test/rules.js
@@ -6,6 +6,30 @@ var quote = require('shell-quote').quote;
 
 // Define test utilities
 var testUtils = {
+  precheck: function (filepath) {
+    // Lint our file with both JSCS and JSHint
+    before(function lintFn (done) {
+      // Generate our command
+      var jshintCmd = require.resolve('jshint/bin/jshint');
+      var cmd = quote([jshintCmd, '--config', __dirname + '/../lib/rc/jshint-critical.json', filepath]);
+
+      // Run the command and save our results
+      var that = this;
+      exec(cmd, function handleExec (err, stdout, stderr) {
+        that.err = err;
+        that.stdout = stdout;
+        that.stderr = stderr;
+        done();
+      });
+    });
+
+    // Clean up results
+    after(function cleanup () {
+      delete this.err;
+      delete this.stdout;
+      delete this.stderr;
+    });
+  },
   lint: function (filepath) {
     // Lint our file with both JSCS and JSHint
     before(function lintFn (done) {
@@ -57,6 +81,29 @@ glob.sync('node/valid-*.js', {cwd: __dirname + '/test-files/'}).forEach(function
 
       it('has no errors', function () {
         expect(this.err).to.equal(null);
+      });
+    });
+  });
+});
+
+glob.sync('node/precheck-*.js', {cwd: __dirname + '/test-files/'}).forEach(function checkInvalidFile (_filepath) {
+  var filepath = __dirname + '/test-files/' + _filepath;
+  describe('A precheck valid file "' + _filepath + '"', function () {
+    describe('when prechecked', function () {
+      testUtils.precheck(filepath);
+
+      it('has no errors', function () {
+        expect(this.err).to.equal(null);
+      });
+    });
+
+    describe('when linted', function () {
+      testUtils.lint(filepath);
+
+      it('receives its expected error', function () {
+        var expected = require(filepath).expected;
+        expect(this.err).to.not.equal(null);
+        expect(this.stdout).to.match(expected);
       });
     });
   });
